bf77f0d190f442be50df23eb4aa4854fd49a526a
Kaiwen <test@test.com>
2023-12-18 19:58:09 +0000

    added glowing blob

---
+++ components/GlowingBlob.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+
+const GlowingBlob = () => {
+    return (
+        <>
+            <div id="blob"> </div>
+            <div id="blur"> </div>
+        </> 
+  )
+}
+
+export default GlowingBlob
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import GlowingBlob from "@/components/GlowingBlob";
 import SideBar from "@/components/SideBar";
 
 
 export default function Home() {
   return(
     <div className="flex">
+      <GlowingBlob />
       <SideBar />
     </div>
   )
